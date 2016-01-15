a73799ec028122ef4dfc3b01e05522d7dceb52a8
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-01-14 23:58:30 -0800

    added POST requst

--- clientstrong/app/src/main/java/v3/clientstrong/MainActivity.java
+++ clientstrong/app/src/main/java/v3/clientstrong/MainActivity.java
@@ -15,8 +15,11 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,8 +42,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ImageView imageView = (ImageView) findViewById(R.id.memberListIcon);
-        imageView.setOnClickListener(new View.OnClickListener() {
+
+        /**
+         * Opens up new Main Activity;
+         */
+
+        ImageView newActivity = (ImageView) findViewById(R.id.memberListIcon);
+        newActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -49,38 +57,112 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final TextView mTextView = (TextView) findViewById(R.id.text);
 
-        String url = "https://warm-refuge-4462.herokuapp.com/api/v1/members";
+        /**
+         * Processes GET request to get JSONArray;
+         */
+        ImageView getRequest = (ImageView) findViewById(R.id.programListIcon);
 
-        JsonArrayRequest jsObjRequest = new JsonArrayRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        mTextView.setText("Response: " + response.toString());
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        mTextView.setText("Response: " + error.toString());
-
-                    }
-                }){
-
-            // Overrides header; necessary to set response type;
+        getRequest.setOnClickListener(new View.OnClickListener() {
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String>  params = new HashMap<>();
-                params.put("Accept", "application/json");
+            public void onClick(View v) {
 
-                return params;
+                final TextView mTextView = (TextView) findViewById(R.id.text);
+
+                String url = "https://warm-refuge-4462.herokuapp.com/api/v1/members";
+
+                JsonArrayRequest jsObjRequest = new JsonArrayRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                mTextView.setText("Response: " + response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO Auto-generated method stub
+                                mTextView.setText("Response: " + error.toString());
+
+                            }
+                        }) {
+
+                    // Overrides header; necessary to set response type;
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Accept", "application/json");
+
+                        return params;
+                    }
+                };
+
+                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
             }
-        };
+        });
 
-        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+        /**
+         * Processes POST request to get JSONArray;
+         */
+        ImageView postRequest = (ImageView) findViewById(R.id.invoiceIcon);
+
+        postRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final TextView mTextView = (TextView) findViewById(R.id.text);
+
+
+                String url = "https://warm-refuge-4462.herokuapp.com/api/v1/members";
+
+                Map<String, Object> params = new HashMap<String, Object>();
+                    params.put("email", "snowwhite5@cs.ca");
+                    params.put("password", "password");
+                    params.put("first_name", "snow12");
+                    params.put("last_name", "white12");
+                    params.put("address", "street somewhere");
+                    params.put("mobile", "234-234-3456");
+                    params.put("birthday", "1902-12-02");
+
+                Map<String, Object> member = new HashMap<String, Object>();
+                member.put("member", params);
+                member.put("role_id", 2);
+
+                    JSONObject jsonBody = new JSONObject(member);
+
+                JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                        (url, jsonBody, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                mTextView.setText("Response: " + response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO Auto-generated method stub
+                                mTextView.setText("Response: " + error.toString());
+
+                            }
+                        }) {
+
+                    // Overrides header; necessary to set response type;
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Accept", "application/json");
+
+                        return params;
+                    }
+                };
+
+                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
+            }
+        });
+
+
     }
 }
 
