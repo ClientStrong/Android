61572829339a0af8e922ae98e75b9103bc43af80
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-10-15 14:48:20 -0700

    App ToolBar Custom menus

--- clientstrong/app/src/main/java/v3/clientstrong/activities/ProfileScreen.java
+++ clientstrong/app/src/main/java/v3/clientstrong/activities/ProfileScreen.java
@@ -1,11 +1,15 @@
 package v3.clientstrong.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import v3.clientstrong.R;
 
@@ -18,7 +22,7 @@ public class ProfileScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-        setTitle("Member Profile");
+        setTitle("Member");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +33,48 @@ public class ProfileScreen extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile_screen, menu);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            Drawable email = getDrawable(R.drawable.ic_email);
+            Drawable phone = getDrawable(R.drawable.ic_phone);
+            email.setAlpha(100);
+            phone.setAlpha(100);
+            menu.findItem(R.id.action_email).setIcon(email);
+            menu.findItem(R.id.action_call).setIcon(phone);
+        }
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_email:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this, "Send email", Toast.LENGTH_SHORT).show();
+
+
+                return true;
+
+            case R.id.action_call:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, "Make a call", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 }
